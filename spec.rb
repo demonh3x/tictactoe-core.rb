79f3739806d464cb8838dc94a40f74113e734b88
Mateu Adsuara <mateuadsuara@gmail.com>
2015-03-09 16:58:04 +0000

    Some basic functionality in Game

--- spec.rb
+++ spec.rb
@@ -36,14 +36,19 @@ class Game
   def initialize(initial_state, players)
     raise Error if (players.size != 2 || initial_state.size != 9) 
     @state = initial_state
+    @players = players
   end
   def advance()
-    @state = ['X', nil, nil, nil, nil, nil, nil, nil, nil]
+    p = @players.cycle.peek
+    player = p[1]
+    icon = p[0]
+    @state = next_state(@state, player.play, icon) 
   end
   def state()
     @state
   end
 end
+
 describe "Game" do
   before(:each) do
     @empty_state = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
@@ -65,11 +70,16 @@ describe "Game" do
     expect(game.state).to eq(@empty_state)
   end
   
-  it "Advances asking the first player" do
-    allow(@players['X']).to receive(:play).and_return(0)
+  def first_play_test(player_icon, location, expected_state)
+    allow(@players[player_icon]).to receive(:play).and_return(location)
     game = Game.new(@empty_state, @players)
 
     game.advance()
-    expect(game.state).to eq(['X', nil, nil, nil, nil, nil, nil, nil, nil])
+    expect(game.state).to eq(expected_state)
+  end
+
+  it "Advances asking the first player" do
+    first_play_test('X', 0, ['X', nil, nil, nil, nil, nil, nil, nil, nil])
+    first_play_test('X', 1, [nil, 'X', nil, nil, nil, nil, nil, nil, nil])
   end
 end
