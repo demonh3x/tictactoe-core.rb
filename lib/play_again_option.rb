498830a0ff8b91acd9f9f8279395dd46552ff9d5
Mateu Adsuara <mateuadsuara@gmail.com>
2015-03-18 12:22:01 +0000

    Add PlayAgainOption

---
+++ lib/play_again_option.rb
@@ -0,0 +1,16 @@
+class PlayAgainOption
+  def initialize(input, output)
+    @option = CliOptions.new(input, output)
+  end
+
+  def ask
+    response = option.ask_for_one(
+      "Do you want to play again?",
+      {"y" => "Yes", "n" => "No"})
+
+    response === "y"
+  end
+
+  private
+  attr_accessor :option
+end
---
+++ lib/play_again_option_spec.rb
@@ -0,0 +1,40 @@
+RSpec.describe "Play again option" do
+  def cli_output(commands)
+    input = commands.push("").join("\n")
+    out = StringIO.new
+    option = PlayAgainOption.new(StringIO.new(input), out)
+    option.ask
+    out.string
+  end
+
+  it "should print the question" do
+    expect(cli_output %w(y)).to include("Do you want to play again?")
+  end
+
+  it "should print the options" do
+    expect(cli_output %w(y)).to include("y = Yes\nn = No")
+  end
+
+  it "given an invalid option should say is invalid" do
+    expect(cli_output %w(z y)).to include('"z" is not a valid option!')
+  end
+
+  it "given an invalid option should repeat the valid ones" do
+    expect(cli_output %w(z y)).to include('try one of ["y", "n"]')
+  end
+
+  def play_again?(commands)
+    input = commands.push("").join("\n")
+    out = StringIO.new
+    option = PlayAgainOption.new(StringIO.new(input), out)
+    option.ask
+  end
+
+  it "should return true if answering yes" do
+    expect(play_again? %w(y)).to eq(true)
+  end
+
+  it "should return false if answering no" do
+    expect(play_again? %w(n)).to eq(false)
+  end
+end
