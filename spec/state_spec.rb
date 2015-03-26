0c6bc945afb65b9c134b58cd71a790b61c7117cb
Mateu Adsuara <mateuadsuara@gmail.com>
2015-03-26 11:02:06 +0000

    Moved game tests to state

--- spec/state_spec.rb
+++ spec/state_spec.rb
@@ -12,4 +12,102 @@ RSpec.describe "Game state" do
     initial_state.put(2, :mark)
     expect(initial_state.look_at(2)).to eq(nil)
   end
+
+  describe "given a 3x3 board" do
+    before(:each) do
+      @board = ThreeByThreeBoard.new
+      @state = State.new(@board)
+    end
+
+    def expect_state(marks)
+        marks.each_with_index do |mark, location|
+          expect(@state.look_at location).to eq(mark)
+        end
+    end
+
+    describe "with no moves" do
+      it "should have an empty state" do
+        expect_state([nil, nil, nil, nil, nil, nil, nil, nil, nil])
+      end
+
+      it "should not be finished" do
+        expect(@state.is_finished?).to eq(false)
+      end
+
+      it "should have no winner" do
+        expect(@state.winner).to eq(nil)
+      end
+    end
+
+    describe "with the first move" do
+      before(:each) do
+        @loc = 0
+        @state = @state.put(@loc, :X)
+      end
+
+      it "should contain that move" do
+        expect(@state.look_at(@loc)).to eq(:X)
+      end
+    end
+
+    ThreeByThreeBoard.new.lines.each do |line|
+      describe "with a line for player a" do
+        before(:each) do
+          line.each do |l|
+            @state = @state.put(l, :X)
+          end
+        end
+
+        it "should be finished" do
+          expect(@state.is_finished?).to eq(true)
+        end
+
+        it "should have won" do
+          expect(@state.winner).to eq(:X)
+        end
+      end
+    end
+
+    def set_state(*marks)
+      marks.each_with_index do |mark, location|
+        @state = @state.put(location, mark)
+      end
+    end
+
+    describe "with three moves not in line for player a" do
+      before(:each) do
+        set_state(
+          :X,  :X,  nil,
+          :X,  nil, nil,
+          nil, nil, nil
+        )
+      end
+
+      it "should not be finished" do
+        expect(@state.is_finished?).to eq(false)
+      end
+
+      it "should have no winner" do
+        expect(@state.winner).to eq(nil)
+      end
+    end
+
+    describe "with a full board but no winner" do
+      before(:each) do
+        set_state(
+          :O, :X, :X,
+          :X, :X, :O,
+          :O, :O, :X
+        )
+      end
+
+      it "should be finished" do
+        expect(@state.is_finished?).to eq(true)
+      end
+
+      it "should have no winner" do
+        expect(@state.winner).to eq(nil)
+      end
+    end
+  end
 end
