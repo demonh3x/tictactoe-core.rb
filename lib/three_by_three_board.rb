9137dcad049f68ef953f986b4091e0d5197326ec
Mateu Adsuara <mateuadsuara@gmail.com>
2015-03-30 12:00:07 +0100

    ThreeByThreeBoard has constant values

--- lib/three_by_three_board.rb
+++ lib/three_by_three_board.rb
@@ -1,18 +1,16 @@
 class ThreeByThreeBoard
-  def initialize
-    @locations = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  LOCATIONS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-    horizontal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-    vertical = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
-    diagonals = [[0, 4, 8], [2, 4, 6]]
-    @lines = horizontal + vertical + diagonals
-  end
+  HORIZONTAL_LINES = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+  VERTICAL_LINES = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+  DIAGONALS = [[0, 4, 8], [2, 4, 6]]
+  LINES = HORIZONTAL_LINES + VERTICAL_LINES + DIAGONALS
 
   def locations
-    @locations
+    LOCATIONS
   end
 
   def lines
-    @lines
+    LINES
   end
 end
