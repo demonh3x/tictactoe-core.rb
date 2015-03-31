bbb0d161b7ade008174e29cf04883c4232818829
Mateu Adsuara <mateuadsuara@gmail.com>
2015-03-31 11:17:41 +0100

    Replace custom Stubs by RSpec spies

--- spec/random_player_spec.rb
+++ spec/random_player_spec.rb
@@ -2,31 +2,15 @@ require 'state'
 require 'random_player'
 
 RSpec.describe "Random player" do
-  class BoardStub
-    def locations
-      [0, 1, 2]
-    end
-  end
-
-  class RandomStub
-    def initialize(*values_to_return)
-      @random_values = *values_to_return
-    end
-
-    def rand
-      @random_values.pop
-    end
-  end
-
   it "has a mark" do
-    player = RandomPlayer.new(:mark, RandomStub.new)
+    player = RandomPlayer.new(:mark, nil)
     expect(player.mark).to eq(:mark)
   end
 
   describe "given only one possibility" do
     before(:each) do
-      @random_player = RandomPlayer.new(:mark, RandomStub.new(0.9))
-      @state = State.new(BoardStub.new)
+      @random_player = RandomPlayer.new(:mark, spy("random", :rand => 0.9))
+      @state = State.new(spy("board", :locations => [0, 1, 2]))
       @state = @state.put(0, :player_x)
       @state = @state.put(1, :player_x)
     end
@@ -39,18 +23,18 @@ RSpec.describe "Random player" do
 
   describe "given two possibilities" do
     before(:each) do
-      @state = State.new(BoardStub.new)
+      @state = State.new(spy("board", :locations => [0, 1, 2]))
       @state = @state.put(0, :player_x)
     end
 
     it "if the random selects the first location, returns it" do
-      @random_player = RandomPlayer.new(:mark, RandomStub.new(0.0))
+      @random_player = RandomPlayer.new(:mark, spy("random", :rand => 0.0))
       loc = @random_player.ask_for_location(@state)
       expect(loc).to eq 1
     end
 
     it "if the random selects the second location, returns it" do
-      @random_player = RandomPlayer.new(:mark, RandomStub.new(0.999))
+      @random_player = RandomPlayer.new(:mark, spy("random", :rand => 0.999))
       loc = @random_player.ask_for_location(@state)
       expect(loc).to eq 2
     end
