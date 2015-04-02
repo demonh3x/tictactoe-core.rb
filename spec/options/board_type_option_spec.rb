9fce571815e94a8c02693ee34cd6f60d7337c1fa
Mateu Adsuara <mateuadsuara@gmail.com>
2015-04-02 16:46:03 +0100

    Add individual tests for BoardTypeOption

---
+++ spec/options/board_type_option_spec.rb
@@ -0,0 +1,25 @@
+require 'options/board_type_option'
+
+RSpec.describe BoardTypeOption do
+  before(:each) do
+    @selection = spy(:read => :selection_value)
+    @factory = spy(:create => :created_value)
+    @option = described_class.new @selection, @factory
+  end
+
+  describe "when getting" do
+    it 'reads the selection' do
+      @option.get
+      expect(@selection).to have_received(:read)
+    end
+
+    it 'sends the selection to the factory' do
+      @option.get
+      expect(@factory).to have_received(:create).with(:selection_value)
+    end
+
+    it 'returns the creation from the factory' do
+      expect(@option.get).to eq(:created_value)
+    end
+  end
+end
