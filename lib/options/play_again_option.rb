3c13bc59e21667b49e72d8c09f78e5eb83384342
Mateu Adsuara <mateuadsuara@gmail.com>
2015-04-02 15:53:10 +0100

    Improve PlayAgainOption by using constants

--- lib/options/play_again_option.rb
+++ lib/options/play_again_option.rb
@@ -1,12 +1,15 @@
 class PlayAgainOption
   def initialize(cli)
     @option = cli
+  QUESTION = "Do you want to play again?"
+  OPTIONS = {"y" => "Yes", "n" => "No"}
   end
 
   def get
     response = option.ask_for_one(
       "Do you want to play again?",
       {"y" => "Yes", "n" => "No"})
+    response = option.ask_for_one(QUESTION, OPTIONS)
 
     response === "y"
   end
