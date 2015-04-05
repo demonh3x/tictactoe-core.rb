4bdd186ab803f3d7afd1e23bfe60469c05bb0673
Mateu Adsuara <mateuadsuara@gmail.com>
2015-04-05 22:39:07 +0100

    Add commas to last elements of multiline lists

--- lib/boards/three_by_three_board.rb
+++ lib/boards/three_by_three_board.rb
@@ -4,7 +4,7 @@ class ThreeByThreeBoard
   HORIZONTAL_LINES = [
     [0, 1, 2],
     [3, 4, 5],
-    [6, 7, 8]
+    [6, 7, 8],
   ]
   VERTICAL_LINES = [
     [0, 3, 6],
@@ -13,7 +13,7 @@ class ThreeByThreeBoard
   ]
   DIAGONALS = [
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ]
   LINES = HORIZONTAL_LINES + VERTICAL_LINES + DIAGONALS
 
