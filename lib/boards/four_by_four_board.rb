b784a2d694df80f56106aae5b6d46becdefbcfcb
Mateu Adsuara <mateuadsuara@gmail.com>
2015-04-05 22:27:48 +0100

    Improve symmetry in boards

--- lib/boards/four_by_four_board.rb
+++ lib/boards/four_by_four_board.rb
@@ -1,19 +1,23 @@
 class FourByFourBoard
-  LOCATIONS = (0..((4*4) -1)).to_a
-  LINES = [
+  LOCATIONS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+  HORIZONTAL_LINES = [
     [0, 1, 2, 3],
     [4, 5, 6, 7],
     [8, 9, 10, 11],
     [12, 13, 14, 15],
-
+  ]
+  VERTICAL_LINES = [
     [0, 4, 8, 12],
     [1, 5, 9, 13],
     [2, 6, 10, 14],
     [3, 7, 11, 15],
-
-    [0, 5, 10, 15],
-    [3, 6, 9, 12]
   ]
+  DIAGONALS = [
+    [0, 5, 10, 15],
+    [3, 6, 9, 12],
+  ]
+  LINES = HORIZONTAL_LINES + VERTICAL_LINES + DIAGONALS
 
   def locations
     LOCATIONS
