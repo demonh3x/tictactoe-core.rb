6460ceddec4e62753ee9020dbca12937f40543d2
Mateu Adsuara <mateuadsuara@gmail.com>
2015-04-09 13:03:26 +0100

    ABMinimax refactor

--- lib/players/ai/ab_minimax.rb
+++ lib/players/ai/ab_minimax.rb
@@ -11,26 +11,17 @@ class ABMinimax
 
     tree.childs.each do |child|
       if !is_leaf?(child)
-        if tree.childs.size == 1
-          a = tree.childs[0]
-          return [a]
-        end
-
-        if tree.childs.size == 2
-          a = tree.childs[0]
-          b = tree.childs[1]
-          choice = a if is_leaf? a
-          choice = b if is_leaf? b
-          return [choice]
-        end
+        score = -1
+      else
+        score = child.score
       end
 
-      if child.score == best_score
+      if score == best_score
         best_nodes << child
       end
 
-      if child.score > best_score
-        best_score = child.score
+      if score > best_score
+        best_score = score
         best_nodes = [child]
       end
     end
