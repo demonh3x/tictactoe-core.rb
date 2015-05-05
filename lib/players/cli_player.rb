b72ce3ae4e1efeae2239d35215149fa9f8747424
Mateu Adsuara <mateuadsuara@gmail.com>
2015-05-05 17:12:33 +0100

    Separates MoveReader class from CliPlayer

--- lib/players/cli_player.rb
+++ lib/players/cli_player.rb
@@ -1,3 +1,5 @@
+require 'uis/cli/move_reader'
+
 module Players
   class CliPlayer
     def initialize(mark, input, output)
@@ -15,7 +17,6 @@ module Players
     end
 
     def play
-      print_turn
       state.make_move(read_valid_location(state), mark)
     end
 
@@ -26,51 +27,18 @@ module Players
     attr_reader :input, :output
     attr_accessor :state
 
-    def print_turn
-      output.puts "Your turn! Where do you want to play?"
-    end
-
     def read_valid_location(state)
-      begin
-        location = read_location
-        available = available? state, location 
-        print_not_available if !available
-      end until available
+      UIs::Cli::MoveReader.new(input, output, StateAdapter.new(state)).get_move!
+    end
+  end
 
-      location
+  class StateAdapter
+    def initialize(state)
+      @state = state
     end
 
-    def available?(state, location)
-      state.available_moves.include? location
-    end
-
-    def read_location
-      begin
-        input_string = read_input
-        location = parse_location(input_string)
-        invalid_input = location.nil?
-        print_invalid_input(input_string) if invalid_input
-      end while invalid_input
-
-      location
-    end
-
-    def read_input
-      str = input.gets
-      raise "No data readed from the CLI input!" if str.nil?
-      str.chomp
-    end
-
-    def parse_location(location_string)
-      Integer(location_string) rescue nil
-    end
-
-    def print_invalid_input(input_string)
-      output.puts "Don't understand \"#{input_string}\". Please, make sure you use a number."
-    end
-
-    def print_not_available
-      output.puts "That location is not available. Please, try another one.\n"
+    def available
+      @state.available_moves
     end
   end
 end
