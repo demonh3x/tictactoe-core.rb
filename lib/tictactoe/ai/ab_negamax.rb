a218716151d3ed2ec1b0953b6bfdcfaa53736cd8
Mateu Adsuara <mateuadsuara@gmail.com>
2015-05-18 15:16:09 +0100

    Adds negamax algorithm

---
+++ lib/tictactoe/ai/ab_negamax.rb
@@ -0,0 +1,41 @@
+module Tictactoe
+  module Ai
+    class ABNegamax
+      COLOR_SELF = 1
+      COLOR_OPPONENT = -1
+
+      def initialize(depth_limit, depth_reached_score)
+        @depth_limit = depth_limit
+        @depth_reached_score = depth_reached_score
+      end
+
+      attr_reader :depth_reached_score, :depth_limit
+
+      def evaluate(tree)
+        negamax(tree, depth_limit, -1000, 1000, COLOR_SELF)
+      end
+
+      def negamax(node, depth, a, b, color)
+        return {:score => color * node.score, :nodes => []} if node.is_leaf?
+        return {:score => color * depth_reached_score, :nodes => []} if depth == 0
+
+        best_score = -1000
+        best_nodes = []
+        node.childs.each do |child|
+          score = -negamax(child, depth-1, -b, -a, -color)[:score]
+          if score > best_score
+            best_score = score
+            best_nodes = [child]
+          elsif score == best_score
+            best_nodes << child
+          end
+          a = [a, score].max
+          break if a >= b
+        end
+
+        {:score => best_score, :nodes => best_nodes}
+      end
+
+    end
+  end
+end
