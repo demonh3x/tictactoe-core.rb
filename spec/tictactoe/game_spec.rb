2c9547ab253cd9080dafe002731990b0911d6614
Mateu Adsuara <mateuadsuara@gmail.com>
2015-05-21 11:25:12 +0100

    Renames variable to correct name: game

--- spec/tictactoe/game_spec.rb
+++ spec/tictactoe/game_spec.rb
@@ -17,37 +17,38 @@ RSpec.describe Tictactoe::Game do
     end
   end
 
+
   let(:moves_source) {MovesSource.new}
 
   def create(board_size, x_type, o_type)
-    ttt = described_class.new(board_size, x_type, o_type)
-    ttt.user_moves = moves_source
-    ttt
+    game = described_class.new(board_size, x_type, o_type)
+    game.user_moves = moves_source
+    game
   end
 
-  def human_tick_playing_to(ttt, loc)
+  def human_tick_playing_to(game, loc)
     moves_source.add(loc)
-    ttt.tick()
+    game.tick()
   end
   
-  def computer_tick(ttt)
-    ttt.tick()
+  def computer_tick(game)
+    game.tick()
   end
 
-  def expect_amount_of_marks(ttt, mark, expected_count)
-    actual_count = ttt.marks.select{|m| m == mark}.count
+  def expect_amount_of_marks(game, mark, expected_count)
+    actual_count = game.marks.select{|m| m == mark}.count
     expect(actual_count).to eq(expected_count)
   end
 
   it 'is not finished' do
-    ttt = create(3, :human, :human)
-    expect(ttt.is_finished?).to eq(false)
+    game = create(3, :human, :human)
+    expect(game.is_finished?).to eq(false)
   end
 
   describe 'can be observed' do
     it 'initial game size 3' do
-      ttt = create(3, :human, :human)
-      expect(ttt.marks).to eq([
+      game = create(3, :human, :human)
+      expect(game.marks).to eq([
         nil, nil, nil,
         nil, nil, nil,
         nil, nil, nil
@@ -55,8 +56,8 @@ RSpec.describe Tictactoe::Game do
     end
 
     it 'initial game size 4' do
-      ttt = create(4, :human, :human)
-      expect(ttt.marks).to eq([
+      game = create(4, :human, :human)
+      expect(game.marks).to eq([
         nil, nil, nil, nil,
         nil, nil, nil, nil,
         nil, nil, nil, nil,
@@ -65,9 +66,9 @@ RSpec.describe Tictactoe::Game do
     end
 
     it 'first play of x on size 3' do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      expect(ttt.marks).to eq([
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 0)
+      expect(game.marks).to eq([
         :x,  nil, nil,
         nil, nil, nil,
         nil, nil, nil
@@ -75,10 +76,10 @@ RSpec.describe Tictactoe::Game do
     end
     
     it 'first play of o on size 3' do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 1)
-      expect(ttt.marks).to eq([
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 1)
+      expect(game.marks).to eq([
         :x,  :o,  nil,
         nil, nil, nil,
         nil, nil, nil
@@ -86,30 +87,30 @@ RSpec.describe Tictactoe::Game do
     end
   end
 
-  describe 'has the available locations' do
-    it do
-      ttt = create(3, :human, :human)
-      expect(ttt.available).to eq([
+  describe 'available' do
+    it 'returns all the locations when no move has been made' do
+      game = create(3, :human, :human)
+      expect(game.available).to eq([
         0, 1, 2,
         3, 4, 5,
         6, 7, 8
       ])
     end
 
-    it do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      expect(ttt.available).to eq([
+    it 'returns all the available locations for when there is one move made on 3 by 3' do
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 0)
+      expect(game.available).to eq([
            1, 2,
         3, 4, 5,
         6, 7, 8
       ])
     end
 
-    it do
-      ttt = create(4, :human, :human)
-      human_tick_playing_to(ttt, 15)
-      expect(ttt.available).to eq([
+    it 'returns all the available locations for when there is one move made on 4 by 4' do
+      game = create(4, :human, :human)
+      human_tick_playing_to(game, 15)
+      expect(game.available).to eq([
         0, 1, 2, 3,
         4, 5, 6, 7,
         8, 9,10,11,
@@ -120,91 +121,91 @@ RSpec.describe Tictactoe::Game do
 
   describe 'is not finished if no player has a line' do
     it do
-      ttt = create(4, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 4)
-      human_tick_playing_to(ttt, 1)
-      human_tick_playing_to(ttt, 5)
-      human_tick_playing_to(ttt, 2)
-      human_tick_playing_to(ttt, 6)
-      expect(ttt.is_finished?).to eq(false)
+      game = create(4, :human, :human)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 4)
+      human_tick_playing_to(game, 1)
+      human_tick_playing_to(game, 5)
+      human_tick_playing_to(game, 2)
+      human_tick_playing_to(game, 6)
+      expect(game.is_finished?).to eq(false)
     end
     
     it do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 3)
-      human_tick_playing_to(ttt, 1)
-      human_tick_playing_to(ttt, 4)
-      expect(ttt.is_finished?).to eq(false)
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 3)
+      human_tick_playing_to(game, 1)
+      human_tick_playing_to(game, 4)
+      expect(game.is_finished?).to eq(false)
     end
   end
 
   describe 'is finished when a player has a line' do
     it do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 3)
-      human_tick_playing_to(ttt, 1)
-      human_tick_playing_to(ttt, 4)
-      human_tick_playing_to(ttt, 2)
-      expect(ttt.is_finished?).to eq(true)
-      expect(ttt.winner).to eq(:x)
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 3)
+      human_tick_playing_to(game, 1)
+      human_tick_playing_to(game, 4)
+      human_tick_playing_to(game, 2)
+      expect(game.is_finished?).to eq(true)
+      expect(game.winner).to eq(:x)
     end
 
     it do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 3)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 4)
-      human_tick_playing_to(ttt, 1)
-      human_tick_playing_to(ttt, 8)
-      human_tick_playing_to(ttt, 2)
-      expect(ttt.is_finished?).to eq(true)
-      expect(ttt.winner).to eq(:o)
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 3)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 4)
+      human_tick_playing_to(game, 1)
+      human_tick_playing_to(game, 8)
+      human_tick_playing_to(game, 2)
+      expect(game.is_finished?).to eq(true)
+      expect(game.winner).to eq(:o)
     end
 
     it do
-      ttt = create(4, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 4)
-      human_tick_playing_to(ttt, 1)
-      human_tick_playing_to(ttt, 5)
-      human_tick_playing_to(ttt, 2)
-      human_tick_playing_to(ttt, 6)
-      human_tick_playing_to(ttt, 3)
-      expect(ttt.is_finished?).to eq(true)
-      expect(ttt.winner).to eq(:x)
+      game = create(4, :human, :human)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 4)
+      human_tick_playing_to(game, 1)
+      human_tick_playing_to(game, 5)
+      human_tick_playing_to(game, 2)
+      human_tick_playing_to(game, 6)
+      human_tick_playing_to(game, 3)
+      expect(game.is_finished?).to eq(true)
+      expect(game.winner).to eq(:x)
     end
   end
 
   describe 'is finished when the board is full' do
     it do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 1)
-      human_tick_playing_to(ttt, 2)
-      human_tick_playing_to(ttt, 5)
-      human_tick_playing_to(ttt, 3)
-      human_tick_playing_to(ttt, 6)
-      human_tick_playing_to(ttt, 4)
-      human_tick_playing_to(ttt, 8)
-      human_tick_playing_to(ttt, 7)
-      expect(ttt.marks).to eq([
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 1)
+      human_tick_playing_to(game, 2)
+      human_tick_playing_to(game, 5)
+      human_tick_playing_to(game, 3)
+      human_tick_playing_to(game, 6)
+      human_tick_playing_to(game, 4)
+      human_tick_playing_to(game, 8)
+      human_tick_playing_to(game, 7)
+      expect(game.marks).to eq([
         :x, :o, :x,
         :x, :x, :o,
         :o, :x, :o
       ])
-      expect(ttt.is_finished?).to eq(true)
+      expect(game.is_finished?).to eq(true)
     end
   end
 
   describe 'cant play twice to the same location' do
     it 'second play' do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 0)
-      expect(ttt.marks).to eq([
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 0)
+      expect(game.marks).to eq([
         :x,  nil, nil,
         nil, nil, nil,
         nil, nil, nil
@@ -214,17 +215,17 @@ RSpec.describe Tictactoe::Game do
 
   describe 'cant be played when finished' do
     it do
-      ttt = create(3, :human, :human)
+      game = create(3, :human, :human)
 
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, 3)
-      human_tick_playing_to(ttt, 1)
-      human_tick_playing_to(ttt, 4)
-      human_tick_playing_to(ttt, 2)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, 3)
+      human_tick_playing_to(game, 1)
+      human_tick_playing_to(game, 4)
+      human_tick_playing_to(game, 2)
 
-      human_tick_playing_to(ttt, 5)
+      human_tick_playing_to(game, 5)
 
-      expect(ttt.marks).to eq([
+      expect(game.marks).to eq([
         :x,  :x,  :x,
         :o,  :o,  nil,
         nil, nil, nil
@@ -234,58 +235,58 @@ RSpec.describe Tictactoe::Game do
 
   describe 'if the human has no move ignores the ticks' do
     it do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, nil)
-      human_tick_playing_to(ttt, nil)
-      human_tick_playing_to(ttt, nil)
-      expect_amount_of_marks(ttt, :x, 0)
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, nil)
+      human_tick_playing_to(game, nil)
+      human_tick_playing_to(game, nil)
+      expect_amount_of_marks(game, :x, 0)
     end
 
     it do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, nil)
-      human_tick_playing_to(ttt, nil)
-      human_tick_playing_to(ttt, nil)
-      human_tick_playing_to(ttt, 0)
-      expect_amount_of_marks(ttt, :x, 1)
-      expect_amount_of_marks(ttt, :o, 0)
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, nil)
+      human_tick_playing_to(game, nil)
+      human_tick_playing_to(game, nil)
+      human_tick_playing_to(game, 0)
+      expect_amount_of_marks(game, :x, 1)
+      expect_amount_of_marks(game, :o, 0)
     end
 
     it do
-      ttt = create(3, :human, :human)
-      human_tick_playing_to(ttt, 0)
-      human_tick_playing_to(ttt, nil)
-      human_tick_playing_to(ttt, nil)
-      human_tick_playing_to(ttt, nil)
-      expect_amount_of_marks(ttt, :x, 1)
-      expect_amount_of_marks(ttt, :o, 0)
+      game = create(3, :human, :human)
+      human_tick_playing_to(game, 0)
+      human_tick_playing_to(game, nil)
+      human_tick_playing_to(game, nil)
+      human_tick_playing_to(game, nil)
+      expect_amount_of_marks(game, :x, 1)
+      expect_amount_of_marks(game, :o, 0)
     end
   end
 
   describe 'the computer plays' do
     it 'the first turn' do
-      ttt = create(3, :computer, :human)
-      computer_tick(ttt)
-      expect_amount_of_marks(ttt, :x, 1)
-      expect_amount_of_marks(ttt, :o, 0)
+      game = create(3, :computer, :human)
+      computer_tick(game)
+      expect_amount_of_marks(game, :x, 1)
+      expect_amount_of_marks(game, :o, 0)
     end
 
     it 'the second turn' do
-      ttt = create(3, :human, :computer)
-      human_tick_playing_to(ttt, 0)
-      computer_tick(ttt)
-      expect_amount_of_marks(ttt, :x, 1)
-      expect_amount_of_marks(ttt, :o, 1)
+      game = create(3, :human, :computer)
+      human_tick_playing_to(game, 0)
+      computer_tick(game)
+      expect_amount_of_marks(game, :x, 1)
+      expect_amount_of_marks(game, :o, 1)
     end
 
     it 'four consecutive turns' do
-      ttt = create(3, :computer, :computer)
-      computer_tick(ttt)
-      computer_tick(ttt)
-      computer_tick(ttt)
-      computer_tick(ttt)
-      expect_amount_of_marks(ttt, :x, 2)
-      expect_amount_of_marks(ttt, :o, 2)
+      game = create(3, :computer, :computer)
+      computer_tick(game)
+      computer_tick(game)
+      computer_tick(game)
+      computer_tick(game)
+      expect_amount_of_marks(game, :x, 2)
+      expect_amount_of_marks(game, :o, 2)
     end
   end
 end
